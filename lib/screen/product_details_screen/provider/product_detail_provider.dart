543f6552699566a5dec42cd7ae966763e76e8773
tiendev2003 <tientc.21it@vku.udn.vn>
2025-01-02 22:25:30 +0700

    first commit

---
+++ lib/screen/product_details_screen/provider/product_detail_provider.dart
@@ -0,0 +1,38 @@
+import 'package:ecommerceapp/models/product.dart';
+import 'package:ecommerceapp/utility/snack_bar_helper.dart';
+import 'package:ecommerceapp/utility/utility_extention.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_cart/flutter_cart.dart';
+import '../../../core/data/data_provider.dart';
+
+class ProductDetailProvider extends ChangeNotifier {
+  final DataProvider _dataProvider;
+  String? selectedVariant;
+  var flutterCart = FlutterCart();
+
+  ProductDetailProvider(this._dataProvider);
+
+  void addToCart(Product product) {
+    if (product.proVariantId!.isNotEmpty && selectedVariant == null) {
+      SnackBarHelper.showErrorSnackBar('Please select a variant');
+      return;
+    }
+    double? price = product.offerPrice != product.price ? product.offerPrice : product.price;
+    flutterCart.addToCart(
+        cartModel: CartModel(
+            productId: '${product.sId}',
+            productName: '${product.name}',
+            productImages: ['${product.images.safeElementAt(0)?.url}'],
+            variants: [ProductVariant(price: price ?? 0, color: selectedVariant)],
+            productDetails: '${product.description}'
+        )
+    );
+    selectedVariant = null;
+    SnackBarHelper.showSuccessSnackBar('Item Added');
+    notifyListeners();
+  }
+
+  void updateUI() {
+    notifyListeners();
+  }
+}
---
+++ lib/widget/custom_network_image.dart
@@ -0,0 +1,38 @@
+import 'package:ecommerceapp/utility/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomNetworkImage extends StatelessWidget {
+  final String imageUrl;
+  final BoxFit fit;
+  final double scale;
+
+  const CustomNetworkImage({
+    super.key,
+    required this.imageUrl,
+    this.fit = BoxFit.cover,
+    this.scale = 1.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    print("$IMAGE_URL/$imageUrl");
+    return Image.network(
+      "$IMAGE_URL/$imageUrl",
+      fit: fit,
+      scale: scale,
+      loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+        if (loadingProgress == null) return child;
+        return Center(
+          child: CircularProgressIndicator(
+            value: loadingProgress.expectedTotalBytes != null
+                ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                : null,  // Progress indicator.
+          ),
+        );
+      },
+      errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+        return const Icon(Icons.error, color: Colors.red);
+      },
+    );
+  }
+}
